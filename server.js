24683463a11eecec02ed96b49aef1d8d8d8a335c
Soner <sonersg@outlook.com>
2024-10-06 13:25:01 +0300

    params and query

--- server.js
+++ server.js
@@ -9,8 +9,29 @@ const posts = [
   { id: 3, post: "three" },
 ];
 
-app.get("/", (req, res) => {
-  res.json(posts);
+// get all posts
+app.get("/api/posts", (req, res) => {
+  const limit = parseInt(req.query.limit);
+  if (!isNaN(limit) && limit > 0) {
+    res.json(posts.slice(0, limit));
+  } else {
+    res.json(posts);
+  }
+});
+
+// get single post
+app.get("/api/posts/:id", (req, res) => {
+  //   console.log(req.params.id);
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    res
+      .status(404)
+      .json({ message: `A post with the id of ${id} couldn't found` });
+  } else {
+    res.status(200).json(post);
+  }
 });
 
 app.listen(port, () => console.log(`server is listening on ${port}`));
